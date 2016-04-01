c2ea942f8d2fa7d064d01bf90aeacb68304e82d1
517850834 <mangolime@qq.com>
2016-04-01 19:56:24 +0800

    task-js-8

---
+++ task-js-8/scripts.js
@@ -0,0 +1,19 @@
+(function () {
+    var inputSource = document.getElementById("inputSource"),
+        leftInBtn = document.getElementById("leftIn"),
+        rightInBtn = document.getElementById("rightIn"),
+        leftOutBtn = document.getElementById("leftOut"),
+        rightOutBtn = document.getElementById("rightOut"),
+        data = [];
+
+    function dataTrim() {
+
+    }
+
+    function clickHandler() {
+        leftInBtn.addEventListener("click", function () {
+
+        });
+    }
+
+})();
